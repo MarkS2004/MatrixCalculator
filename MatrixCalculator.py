effbf763f370d29aa148b39d15800e4b8f61385a
KemalBa <kemalbagci12@gmail.com>
2024-03-05 12:39:28 +0100

    created add Methode

--- MatrixCalculator.py
+++ MatrixCalculator.py
@@ -43,7 +43,18 @@ class MatrixClass:
         
 #=========================AddMethode===========================#
     def __add__(self, other):
-        pass
+
+        if self._rows == other._rows and self._columns == other._columns:       # Checks if Matrices are the same size
+            for i in range (self._rows):                                        # Goes throw the rows of the Matrices  
+                for j in range (self._columns):                                 # Goes throw the columns of the Matrices
+                    self._matrix[i][j] += other._matrix[i][j] 
+
+                    return self
+
+        else:
+            raise ValueError ("The Matrices are not the same size! They can not be added!") # Errror message
+
+    
 
 #=========================SubMethode===========================#
     def __sub__(self, other):
@@ -62,7 +73,6 @@ class MatrixClass:
 ################################################################
 
 #=========================SubCaption===========================#
-
 
 ################################################################
 # Functions                                                    #
